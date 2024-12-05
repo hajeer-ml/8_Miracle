a9d2155468c71fa7f6c98bf39403cd6fe85ee93a
hejer <mlhajer817@gmail.com>
2024-12-05 16:02:26 +0100

    f_c2

--- app/src/main/java/com/example/a8_miracle/settings.java
+++ app/src/main/java/com/example/a8_miracle/settings.java
@@ -1,8 +1,6 @@
 package com.example.a8_miracle;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,19 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        new Handler().postDelayed(() -> {
-                    Intent intent= new Intent(MainActivity.this , spltwo.class);
-                    startActivities(new Intent[]{intent});
-                    finish();
-                }, 4200
-        );
-
+        setContentView(R.layout.activity_settings);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
---
+++ app/src/main/java/com/example/a8_miracle/spltwo.java
@@ -0,0 +1,33 @@
+package com.example.a8_miracle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class spltwo extends AppCompatActivity {
+    Button next;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_spltwo);
+        next=findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                            Intent intent= new Intent(spltwo.this , Login.class);
+                            startActivities(new Intent[]{intent});
+                            finish();
+            }
+        });
+    }
+}
