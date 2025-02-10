9ca77dd34a3bc9c0d3c18f2741bfa643c37ec6bd
hejer <mlhajer817@gmail.com>
2025-02-10 16:24:20 +0100

    *-*

--- app/src/main/java/com/example/a8_miracle/settings.java
+++ app/src/main/java/com/example/a8_miracle/settings.java
@@ -2,23 +2,24 @@ package com.example.a8_miracle;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
-public class settings extends AppCompatActivity {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Settings extends Fragment {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_settings);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View myView= inflater.inflate(R.layout.fragment_settings, container, false);
+        if(container!= null){
+            container.removeAllViews();
+        }
+        return  myView;
     }
 }
